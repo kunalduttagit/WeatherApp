d7f0b6a64f4bed41f177e4eb2bdfae9f3f8f85ed
kunalduttagit <95156803+kunalduttagit@users.noreply.github.com>
2022-11-20 17:47:53 +0530

    Release 2.0
    -Added Dark Mode support
    -Increased security using all https request protocol (SSL) encryption 
    -Fixed AQI Index not working
    -Improved UI elements
    -Better UX choices
    -Fixed Bugs

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
+document.getElementById("searchbar").style.width = "112px";
+
 document.querySelector("input").addEventListener("input", onInput);
 
 function onInput() {
     var spanElm = this.nextElementSibling;
     spanElm.textContent = this.value + 1; // the hidden span takes the value of the input;
-    this.style.width = spanElm.offsetWidth + "px"; // apply width of the span to the input
+    if (spanElm.offsetWidth < 112) this.style.width = 112 + "px";
+    else {
+        this.style.width = spanElm.offsetWidth + "px"; // apply width of the span to the input
+        this.style.transition = "0.15s";
+    }
 }
 
 window.addEventListener("load", () => {
     document.querySelector("input").addEventListener("change", e => {
         let city = e.currentTarget.value;
-        let lat;
-        let lon;
         let iconpack;
         let bigicon = document.querySelector(".weathericon");
         let apiKey = "f4a29c578357e86a7d54cb0063e275d9";
@@ -20,18 +24,18 @@ window.addEventListener("load", () => {
         let humiditydes = document.querySelector(".aboutHumidity");
         let aqides = document.querySelector(".aboutAqi");
         const api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`;
-        const apiconverter = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
+        const apiconverter = `https://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`;
         fetch(apiconverter)
             .then(response => response.json())
             .then(data => {
                 let lat = data[0].lat;
                 let lon = data[0].lon;
-                const apiaqi = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+                const apiaqi = `https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
                 fetch(apiaqi)
                     .then(response => response.json())
                     .then(data => {
                         let { aqi } = data.list[0].main;
-                        if (aqi <= 1) aqides.textContent = "Good";
+                        if (aqi == 1) aqides.textContent = "Good";
                         if (aqi == 2) aqides.textContent = "Fair";
                         if (aqi == 3) aqides.textContent = "Moderate";
                         if (aqi == 4) aqides.innerHTML = "Poor";
@@ -47,6 +51,7 @@ window.addEventListener("load", () => {
                 //then we decide what to do with the data
                 console.log(data);
                 let { temp, humidity } = data.main;
+                //let {visibility} = data.visibility;
                 const { icon, description } = data.weather[0];
                 let { speed } = data.wind;
                 speed = Math.round(speed);
@@ -82,11 +87,35 @@ window.addEventListener("load", () => {
 
                 degree.textContent = temp;
                 winddes.textContent = `${speed} km/h`;
-                humiditydes.textContent = humidity;
+                humiditydes.textContent = `${humidity} %`;
                 bigicon.innerHTML = `
-                <div>
+                <div class = iconNdes>
                     <img class = "bigicon" src="resources/weatherIcons/${iconpack}.svg">
+                     <div class="description">${description}</div>
                 </div>`;
             });
     });
+});
+
+//dark mode toggle
+let toggle = document.querySelector(".checkbox");
+const body = document.querySelector("body");
+const placeholder = document.querySelector(".searchbar");
+
+toggle.addEventListener("change", function () {
+    if (this.checked) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+        body.style.transition = "1s";
+        placeholder.style.borderBottom = "2px solid white";
+        placeholder.style.color = "white";
+        placeholder.style.transition = "1s";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+        body.style.transition = "1s";
+        placeholder.style.borderBottom = "2px solid black";
+        placeholder.style.color = "black";
+        placeholder.style.transition = "1s";
+    }
 });
